c188958b8f0cc5afbb31ab9e15abc23fe62bca3f
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-01-31 22:34:01 -0800

    Add NavBar container to App.jsx

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SignupContainer from './session/signup_container';
 import MainPage from './home/main.jsx';
+import NavBarContainer from './nav_bar/nav_bar_container';
 
 export default () => (
-  <Switch>
+  <div>
+    <Route path='/' component={NavBarContainer} />
     <Route path='/signup' component={SignupContainer} />
-    <Route path='/' component={MainPage} />
-    
-  </Switch>
+  </div>
   
 );
+// <Route path='/' component={MainPage} />
