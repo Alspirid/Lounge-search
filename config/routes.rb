f4dacf3bf46dc0b50b8e16e57cb8e4471398f5f3
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-02-05 16:25:48 -0800

    Add some User show, index routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create]
+    resources :users, only: [:create, :show, :index]
     resource :session, only: [:create, :destroy, :show]
     resources :locations, only: [:index]
     # resources :bookings
