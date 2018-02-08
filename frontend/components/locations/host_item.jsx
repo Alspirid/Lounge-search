341227b1590573b36bf24ccea9fa46f9d7e41c50
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-02-07 21:34:20 -0800

    Add a link to user profile from location show page

--- frontend/components/locations/host_item.jsx
+++ frontend/components/locations/host_item.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HostItem = ({host}) => (
   <li>
-      <div className='user-image'><img src={host.image_url}></img></div>
+      <div className='user-image'>
+        <Link to={`/users/${host.id}`}><img src={host.image_url}></img></Link>
+      </div>
       <div className='host-info'>
         <h3>{host.username}</h3>
         <div>{host.email}</div>
       </div>
-      <div className='about_text'><p>{host.about_text}</p></div>
+      <div className='about_text'>
+      <p>{host.about_text}<br></br></p>
+        <Link
+          to={`/users/${host.id}`}>Details</Link>
+      </div>
   </li>
+
 );
 
 export default HostItem;
