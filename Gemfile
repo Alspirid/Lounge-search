11bcff10fbbee07c05e1c2a91806be0c836c0bda
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-02-09 14:48:02 -0800

    Updated Gemfile, store.js, package.json, webpack.config.js for production

--- Gemfile
+++ Gemfile
@@ -30,12 +30,12 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-gem 'pry-rails'
 gem 'rails_12factor'
 gem 'jquery-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'pry-rails'
   gem 'annotate'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger';
-
+// import logger from 'redux-logger';
 import rootReducer from '../reducers/root';
 import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 export default (preloadedState = {}) => createStore(
   rootReducer, preloadedState,
-  applyMiddleware(thunk, logger)
+  applyMiddleware(...middlewares)
+  // applyMiddleware(thunk, logger)
 );
