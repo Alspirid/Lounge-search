d2a4789c64e97f3c0b8e83fa9dce51a2f0180c28
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-03-08 23:04:35 -0800

    Add booking reducer to root reducer

--- frontend/reducers/root.js
+++ frontend/reducers/root.js
@@ -3,12 +3,14 @@ import sessionReducer from './session';
 import errorReducer from './session_errors_reducer';
 import userReducer from './user';
 import LocationReducer from './location_reducer';
-import reviewReducer from './review_reducer.js';
+import reviewReducer from './review_reducer';
+import bookingReducer from './booking_reducer';
 
 export default combineReducers({
   session: sessionReducer,
   errors: errorReducer,
   users: userReducer,
   locations: LocationReducer,
-  reviews: reviewReducer
+  reviews: reviewReducer,
+  booking: bookingReducer  
 });
