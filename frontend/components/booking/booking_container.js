8cff575cdbba09e463b7e057e50cdec75c16468b
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-03-08 16:39:51 -0800

    Create new folder frontend/component/booking

---
+++ frontend/components/booking/booking_container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { BookingForm } from 'booking_form';
+
+const mapStateToProps = state => {
+
+};
