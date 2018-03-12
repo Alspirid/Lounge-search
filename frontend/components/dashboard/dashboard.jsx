cd4b616d9864cab54eb0b9c95e4d6325c651e131
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-03-12 16:34:34 -0700

    Transform dashboard to add bookings list

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import DashboardLocations from './dashboard_locations';
+import DashboardLeft from './dashboard_left';
+import BookingIndex from './booking_index';
+
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -8,50 +12,26 @@ class Dashboard extends React.Component {
   
   componentDidMount() {
     this.props.fetchUser(this.props.currentUserId);
+    this.props.fetchBookings();
   }
   
   componentWillReceiveProps(nextProps) {
-    
+    // if (nextProps.bookings) {
+    //   console.log(nextProps.bookings);
+    // }
   }
   
   render() {
     let display;
-    let bookings = [];
-    let hostings = [];
     if (this.props.currentUser) {
-      this.hostStatus = this.props.currentUser.is_host ? 'Accepting Guests' : 'Not Accepting Guests ';
-      bookings = this.props.currentUser.bookings;
-      hostings = this.props.currentUser.is_host ? 
-      this.props.currentUser.hostings : [];
       display = (
         <div className='row dashboard-background'>    
         <div className='dashboard'>
-          <div className='dashboard-left'>
-            <ul>
-              <li><img src={this.props.currentUser.image_url}></img></li>
-              <li><h1>{this.props.currentUser.username}</h1></li>
-              <li><p>{this.props.currentUser.about_text}</p></li>
-              <li><h3>{this.props.currentUser.email}</h3></li>
-              <li><h3>Location: {this.props.currentUser.location.area}</h3></li>              
-            </ul>
-            <h2>{this.hostStatus}</h2>    
-          </div>
+          <DashboardLeft currentUser={this.props.currentUser}/>
           <div className='dashboard-right'>
-            <div className='dashboard-locations'>
-              <h1>Explore the city's best spots with locals...</h1>
-              <div className='dashboard-item'>
-                <Link to='/locations/1'><img src='https://italystorie.files.wordpress.com/2015/01/ggbridge_old_coast_guard_station.jpg'></img></Link>
-                <Link to='/locations/2'><img src='http://www.visitcalifornia.com/sites/default/files/styles/welcome_image/public/VCW_D_SFcity_FishermanWharf_1280x642_sized.jpg'></img></Link>
-                <Link to='/locations/3'><img src='https://media-cdn.tripadvisor.com/media/photo-s/06/34/72/78/chinatown.jpg'></img></Link>
-              </div>
-            </div>
-            <div className='dashboard-travel-plans'>
-              <h1>My Travel Plans</h1>
-                <div className='dashboard-item'>
-                  <h2>You don't have any plans yet...</h2>
-                </div>
-
-            </div>
+            <DashboardLocations />
+            <BookingIndex deleteBooking={this.props.deleteBooking} 
+              bookings={this.props.bookings}/>
           </div>
         </div>
         </div>
--- frontend/components/dashboard/dashboard_container.js
+++ frontend/components/dashboard/dashboard_container.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchUser, fetchUsers} from '../../actions/user_actions';
 import Dashboard from './dashboard';
+import { fetchBookings,deleteBooking } from '../../actions/booking_actions';
 
 const mapStateToProps = state => {
   let currentUserId = state.session.currentUser.id;
@@ -9,6 +10,12 @@ const mapStateToProps = state => {
     {
       currentUserId: state.session.currentUser.id,
       currentUser: state.users[currentUserId],
+      bookings: Object.values(state.bookings).filter(element =>{
+        return element.traveler_id === state.session.currentUser.id;
+      }),
+      hostings: Object.values(state.bookings).filter(element =>{
+        return element.host_id === state.session.currentUser.id;
+      }),
     }
   );
 };
@@ -16,7 +23,9 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   fetchUser: (id) => dispatch(fetchUser(id)),
-  fetchUsers: () => dispatch(fetchUsers())
+  fetchUsers: () => dispatch(fetchUsers()),
+  fetchBookings: () => dispatch(fetchBookings()),
+  deleteBooking: id => dispatch(deleteBooking(id)),
 });
 
 export default connect(mapStateToProps,mapDispatchToProps)(Dashboard);
