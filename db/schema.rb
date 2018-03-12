4442c833fd881490fc8883aa3f0dad03b8fe9530
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-03-12 16:30:25 -0700

    Bookings table has been updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208171054) do
+ActiveRecord::Schema.define(version: 20180312181308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180208171054) do
     t.datetime "departure"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "locations", force: :cascade do |t|
