d4417a351651a0b39a3b65d2704a382ad8a57ac5
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-03-13 13:14:46 -0700

    Update pictures sources

--- frontend/components/dashboard/dashboard_locations.jsx
+++ frontend/components/dashboard/dashboard_locations.jsx
@@ -5,9 +5,9 @@ const DashboardLocations = (props) => (
   <div className='dashboard-locations'>
     <h1>Explore the city's best spots with locals...</h1>
     <div className='dashboard-item'>
-      <Link to='/locations/1'><img src='https://italystorie.files.wordpress.com/2015/01/ggbridge_old_coast_guard_station.jpg'></img></Link>
-      <Link to='/locations/2'><img src='http://www.visitcalifornia.com/sites/default/files/styles/welcome_image/public/VCW_D_SFcity_FishermanWharf_1280x642_sized.jpg'></img></Link>
-      <Link to='/locations/3'><img src='https://media-cdn.tripadvisor.com/media/photo-s/06/34/72/78/chinatown.jpg'></img></Link>
+      <Link to='/locations/1'><img src='https://res.cloudinary.com/dgent8r3s/image/upload/v1520227342/presidio.jpg'></img></Link>
+      <Link to='/locations/2'><img src='https://res.cloudinary.com/dgent8r3s/image/upload/v1520227342/richmond.jpg'></img></Link>
+      <Link to='/locations/3'><img src='https://res.cloudinary.com/dgent8r3s/image/upload/v1520227340/fisherman.jpg'></img></Link>
     </div>
   </div>
 );
