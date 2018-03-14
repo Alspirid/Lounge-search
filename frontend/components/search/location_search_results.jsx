33654d3224582b24245fad91c822175579c5e0f1
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-03-14 15:31:34 -0700

    Implement no results found message

--- frontend/components/search/location_search_results.jsx
+++ frontend/components/search/location_search_results.jsx
@@ -4,17 +4,20 @@ import LocatonIndexItem from '../locations/location_index_item';
 class locationSearchResults extends React.Component {
   render(){
     let locations = this.props.locations;
+    let message;
     let locationItems;
     if (locations.length > 0){
+      message = 'Location search results';
       locationItems = locations.map( location =>(
         <LocatonIndexItem key={location.id} location={location} />
       ));
     } else {
+      message = 'No results found';
       locationItems = <div></div>;
     }
   return(
     <div className='row location-background'>
-      <h1 className='location-slogan'>Location search results ...</h1>
+      <h1 className='location-slogan'>{ message }</h1>
         <div className='locations'>
           <ul>
             {locationItems}
